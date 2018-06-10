d22c8046f932820128d0d490a57cebe764273227
rdincher <Ryan@Ryans-MacBook-Air.local>
2018-06-10 15:52:00 -0500

    Panels
    Update to utilize panels to change view on button click

--- Station/src/gui/FrontPage.java
+++ Station/src/gui/FrontPage.java
@@ -1,15 +1,28 @@
 package gui;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
 
-public class FrontPage extends JFrame implements ActionListener{
-	private static final long serialVersionUID = 1L;
+public class FrontPage implements ActionListener{
 
+	JFrame f = new JFrame();
+	JPanel front;
+	JPanel weatherPanel;
+	
 	public FrontPage(){
-		JLabel l = new JLabel();
-		l.setBounds(0, 0, 10, 10);
-		add(l);
+		front = new JPanel();
+		front.setBounds(0,0,480,320);    
+	    front.setBackground(Color.gray);
+	    
+	    weatherPanel = new JPanel();
+	    weatherPanel.setBounds(0,0,480,320);    
+	    weatherPanel.setBackground(Color.gray);
+	    
+	    JLabel l = new JLabel("Testing");
+	    weatherPanel.add(l);
+	    weatherPanel.setVisible(false);
+	    
 		JButton weather = new JButton(new ImageIcon("weather.png"));
 		weather.setBounds(10, 10, 100, 100);
 		weather.addActionListener(this);
@@ -22,25 +35,30 @@ public class FrontPage extends JFrame implements ActionListener{
 		cal.setBounds(210, 10, 100, 100);
 		cal.addActionListener(this);
 		
-		add(weather);
-		add(lights);
-		add(cal);
+		front.add(weather);
+		front.add(lights);
+		front.add(cal);
+		front.setVisible(true);
 		
-		setSize(480, 320);
-		setLayout(null);
-		setVisible(true);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.add(front);
+		f.add(weatherPanel);
+		
+		f.setLayout(null);
+		f.setSize(480,320);  
+        f.setVisible(true);  
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		new FrontPage();
+
+		new FrontPage();  
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+		weatherPanel.setVisible(true);
+		front.setVisible(false);
 	}
 
 }
